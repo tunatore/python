8ea9320cea428114014ac67d105abc25923b2cb6
tuna <tunatore@gmail.com>
2020-02-07 16:08:42 +0100

    pyhton for java developers edits

--- pyhton-for-java-developers.py
+++ pyhton-for-java-developers.py
@@ -46,6 +46,16 @@ boolean2 = 0
 emptyList = []
 emptyCollection = ()
 
+a = b = c = d = 1
+e = 2
+
+print("a b c d e: ", a, b, c, d, e)
+
+var = 0
+var = "a"
+
+print("var: ", var)
+
 # List
 # Unlike Java it can contain different types
 
@@ -53,6 +63,40 @@ list1 = []
 list2 = [1, 2, 3, 4, 5, 6, 7, 8, 100, 200]
 A = [1, 2, 3, 'tuna']
 B = [1, 2, 3, "python", "tuna"]
+C = [1, 2, 3, A, B]
 
+print("C: ", C)
 
+C.append("4")  # list.add()
+C.append("5")  # list.add()
+C.append("6")  # list.add()
 
+print("C: ", C)
+
+print("C[0]: ", C[0])  # list.get(i)
+print("C[4]: ", C[4])   # list.size()
+print("len(C)", len(C))
+
+D = C[:]  # every element in C list.clone()
+print("D:", D)
+
+#tuple
+#immutable
+
+tupleA = ()
+tupleB = (1, 2, 3, 4, 5, 6)
+tupleC = (2, 4, 'A')
+tupleD = (1, 2, tupleB, tupleC)
+
+print("tupleD: ", tupleD)
+print("tupleD[2]", tupleD[2])
+
+#dictionary
+# key - value pairs
+
+people = {'john': 123, "tom": 123}
+
+print("john in people: ", 'john' in people)
+people['john'] = 321
+print(people['john'])
+print(people.keys())
