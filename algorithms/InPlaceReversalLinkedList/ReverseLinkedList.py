acbaf4748a673c0267cfcc48855592ee49e1e0d2
tuna <tunatore@gmail.com>
2020-04-25 14:59:39 +0200

    python reverse linked list

---
+++ algorithms/InPlaceReversalLinkedList/ReverseLinkedList.py
@@ -0,0 +1,44 @@
+#  In place reversal of a LinkedList
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+
+
+# Time complexity O(N) and Space complexity O(1)
+def reverse(head):
+    previous = None
+    current = head
+    while current:
+        next = current.next
+        current.next = previous
+        previous = current
+        current = next
+    return previous
+
+
+def main():
+    head = Node(2)
+    head.next = Node(4)
+    head.next.next = Node(6)
+    head.next.next.next = Node(8)
+    head.next.next.next.next = Node(10)
+
+    print("Nodes of original LinkedList are: ", end='')
+    head.print_list()
+    result = reverse(head)
+    print("Nodes of reversed LinkedList are: ", end='')
+    result.print_list()
+
+
+main()
+
